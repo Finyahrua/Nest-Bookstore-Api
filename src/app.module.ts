7edc181bafd11a235c9472c6f01a1fba1f0cd18e
Finyahrua <finaddy@gmail.com>
2022-11-13 10:22:20 +0300

    generated the cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, CartModule],
+  imports: [AuthModule, ProductModule, CartModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
